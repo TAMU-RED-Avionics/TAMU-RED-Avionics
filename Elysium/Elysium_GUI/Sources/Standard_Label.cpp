8ac6deaa3fbee87938bee56f419fc73816ab7028
simeonshaffar-tamu <simeonshaffar@tamu.edu>
2025-09-12 13:20:06 -0500

    celar

--- Elysium/Elysium_GUI/Sources/Standard_Label.cpp
+++
@@ -1,8 +0,0 @@
-#include "Standard_Label.h"
-#include <Qt>
-
-Standard_Label::Standard_Label(const QString& text, QGridLayout* layout, int row): QLabel() {
-    this->setText(text);
-    this->setAlignment(Qt::AlignLeft | Qt::AlignVCenter);
-    layout->addWidget(this, row, 0);
-}
